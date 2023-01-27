5d9bf04c04c8850e7440b53e9da4c294f35d7af7
Grant Steinke <grantjsteinke@gmail.com>
2023-01-26 20:09:43 -0700

    add persistance provider

--- App.tsx
+++ App.tsx
@@ -1,30 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 
 import {QueryClient, QueryClientProvider} from 'react-query';
-import {MarketPlace} from './app/components/MarketPlace';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Provider} from 'react-redux';
+import {persistor, store} from './app/redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+import {RootNavigation} from './app/navigation/RootNavigation';
 
 const queryClient = new QueryClient();
-
-const Tab = createBottomTabNavigator();
 
 function App(): JSX.Element {
   return (
     <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="MarketPlace" component={MarketPlace} />
-          {/* <Tab.Screen name="Saved" component={SettingsScreen} /> */}
-        </Tab.Navigator>
-      </NavigationContainer>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <NavigationContainer>
+            <RootNavigation />
+          </NavigationContainer>
+        </PersistGate>
+      </Provider>
     </QueryClientProvider>
   );
 }
